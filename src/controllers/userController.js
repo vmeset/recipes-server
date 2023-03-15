7cfaab0257bfd65c514a6bd53c2cf4c7b37fab85
vmeset <vmeset@gmail.com>
2023-03-15 16:05:06 +0300

    controllers created

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,13 @@
-const UserModel = require('../models/Users.js')
+const userModel = require('../models/userModel')
 
 class UserController {
   async register (req, res, next) {
     try {
       const {username, password} = req.body
-      const user = await UserModel.findOne({username})
+      const user = await userModel.findOne({username})
+      if(user) {
+        return res.json('user already exists')
+      }
       res.json(user)
     } catch (e) {
       console.log(e)
@@ -12,10 +15,38 @@ class UserController {
   }
   async getAll (req, res, next) {
     try {
-      const users = await UserModel.find()
+      const users = await userModel.find()
       res.json(users)
     } catch (e) {
       console.log(e)
+    }
+  }
+  async login (req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+  async logout (req, res, next) {
+    try {
+
+    } catch (e) {
+      
+    }
+  }
+  async refresh (req, res, next) {
+    try {
+
+    } catch (e) {
+      
+    }
+  }
+  async activate (req, res, next) {
+    try {
+
+    } catch (e) {
+      
     }
   }
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,6 +6,10 @@ const UserController = require('../controllers/userController')
 const router = new Router()
 
 router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+router.post('/logout', UserController.logout)
 router.get('/fetch', UserController.getAll)
+router.get('/activate/:link', UserController.activate)
+router.get('/refresh', UserController.refresh)
 
 module.exports = router
