1d6ddf493b06cad41ff644279e6475c9589f9cfa
vmeset <vmeset@gmail.com>
2023-03-16 20:27:14 +0300

    test

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true},
-  // image: {type: }
-  // password: {type: String, required: true},
+  username: {type: String, required: true},
+  avatar: {data: Buffer, contentType: String},
+  password: {type: String, required: true},
   // email: {type: String, required: true, unique: true},
   // isActivated: {type: Boolean, default: false},
   // activationLink: {type: String}
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,15 @@
 const Router = require('express')
 const UserController = require('../controllers/userController')
+const imgMiddleware = require('../middleware/imgMiddleware')
 // import jwt from 'jsonwebtoken'
 // import bcrypt from 'bcrypt'
+// const bodyParser = require('body-parser')
 
 const router = new Router()
+// const jsonParser = bodyParser.json()
 
-router.post('/register', UserController.register)
+// router.post('/register', imgMiddleware.single('img'), UserController.register)
+router.post('/register', imgMiddleware.single('avatar'), UserController.register)
 router.post('/login', UserController.login)
 router.post('/logout', UserController.logout)
 router.get('/fetch', UserController.getAll)
