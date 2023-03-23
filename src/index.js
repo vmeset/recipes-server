87b0cb22593bc6e97ff96c1bbf703754c1e77e63
vmeset <vmeset@gmail.com>
2023-03-23 10:40:14 +0300

    cors withCredentials fix

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL
+}));
 app.use('/api', router)
 app.use(express.static(path.resolve(__dirname, '../uploads')))
 app.use(errorHandlingMiddleware)
